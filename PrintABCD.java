92992fd8029b83c03dc911ba46a3e6da069cbdc0
sominath <sominathghuge1@gmail.com>
2025-03-07 12:59:31 +0530

    Logical Program

---
+++ PrintABCD.java
@@ -0,0 +1,16 @@
+package logicalProgram;
+
+public class PrintABCD {
+	
+	public static void main(String[] args) {
+		
+		for (char c='A'; c<='Z'; c++)
+		{
+			System.out.println(c);
+		}
+		
+	
+	
+		
+	}
+}
