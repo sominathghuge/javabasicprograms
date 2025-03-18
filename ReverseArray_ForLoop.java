f6e7865d7a129edc047c4c6fa429de4470f052c7
sominath <sominathghuge1@gmail.com>
2025-03-18 17:46:50 +0530

    ArrayProblem

---
+++ ReverseArray_ForLoop.java
@@ -0,0 +1,23 @@
+package practices;
+
+public class ReverseArray_ForLoop {
+	
+	public static void main(String[] args) {
+		
+	int a[]= {12,13,14,15,16};	
+		
+	for(int i=a.length-1; i>=0; i--)
+	{
+		System.out.println(a[i]);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
